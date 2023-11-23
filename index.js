7212da63427822e8171dccf02a064ec053fbccf0
MarlaRey <hejmarlenehej@gmail.com>
2023-11-23 12:29:44 +0100

    ignore node_modules

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ import http from 'http';
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Hello World!');
+    
     response.end();
 }).listen(4000)
+
+
